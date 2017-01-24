ac40a549c11277ce7a4f9e5a4c9f11117b10735e
Huey <leejinhuey@gmail.com>
2017-01-24 10:57:56 +0800

    API in

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # node-scratchpad
 
 saves everything you give it
+
+built with [Vue.js](https://vuejs.org/), [Express](https://expressjs.com/) and [Bulma](http://bulma.io/)
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var express = require("express");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 var path = require("path");
+var fileCleaner = require("sanitize-filename");
 
 var app = express();
 
+/*
+		app.configuration
+*/
+
+// log all requests to server
 app.use(logger("dev"));
+
+// support JSON-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// serve up all styles & scripts
+app.use("/",express.static(path.join(__dirname, "css")));
+app.use("/",express.static(path.join(__dirname, "scripts")));
+
+/*
+		app.routes
+*/
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/views/index.html"));
@@ -15,13 +35,69 @@ app.get("/about", (req, res) => {
 	res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
-// serve up all styles & scripts
-app.get("/",express.static(path.join(__dirname, "css")));
-app.get("/",express.static(path.join(__dirname, "js")));
-
 // don't clutter up my view files
 app.get("/bulma.css", function(req, res){
 	res.sendFile(path.join(__dirname + "/node_modules/bulma/css/bulma.css"));
+});
+app.get("/bulma.css.map", function(req, res){
+	res.sendFile(path.join(__dirname + "/node_modules/bulma/css/bulma.css.map"));
+});
+app.get("/vue.js", function(req, res){
+	res.sendFile(path.join(__dirname + "/node_modules/vue/dist/vue.js"));
+});
+app.get("/vue-resource.js", function(req, res){
+	res.sendFile(path.join(__dirname + "/node_modules/vue/dist/vue.js"));
+});
+
+/*
+		main logic
+*/
+app.get("/view/all", (req, res) => {
+	fs.readdir(path.join(__dirname,"data"), (err, list) => {
+		var data = [];
+		if(err){
+			return console.log(err);
+		} else {
+			list.forEach((v,i) => {
+				if(path.extname(v) === ".txt"){
+					data.push(v);
+				}
+			});
+		}
+		res.send(JSON.stringify({
+			"files":data
+		}));
+	});
+});
+
+app.get("/view/:secretcode", (req, res) => {
+	var secretcode = req.params.secretcode;
+	var file = fs.readFile(path.join(__dirname,"data",secretcode+".txt"), {encoding:"utf8"},(err, data) => {
+		if(err){
+			return console.log(err);
+		} else {
+			res.send(JSON.stringify(
+				{
+					"message": data
+				}));
+		}
+	});
+});
+
+app.post("/save/:secretcode", (req, res) => {
+	var secretcode = fileCleaner(req.params.secretcode);
+	var contents = req.body.message;
+	var file = fs.writeFile(path.join(__dirname,"data",secretcode+".txt"), contents, {encoding:"utf8"}, (err) => {
+			if(err){
+				return console.log(err);
+			} else {
+				res.send(JSON.stringify({
+					"status":"success",
+					"filename":secretcode
+				}));
+			}
+	});
+	
 });
 
 app.listen(8080, () => {
